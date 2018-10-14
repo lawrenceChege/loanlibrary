99fbb41ede8a4b10d46508abea6baaf8594c82d2
lawrenceChege <lawrencechege.s@gmail.com>
2018-10-14 05:16:46 +0300

    fix class issue

--- src/loanlibrary.ts
+++ src/loanlibrary.ts
@@ -1,4 +1,270 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+
+// import required libraries
+import moment = require("moment")
+
+
+// definitions and variables
+
+// date variables and constants
+let startDate: String
+let repaymentDate: String
+let drawdownDate: any
+
+// loan type one variables
+let dailyOneInterest: number
+let dailyRateOfLoanOne: number
+let balanceOfLoanOne: number
+let loanOneInterestServiced: String
+let loanOneInterestComponded: String
+
+// loan type two variables
+let dailyTwoInterest: number
+let dailyRateOfLoanTwo: number
+let balanceOfLoanTwo: number
+let loanTwoInterestServiced: String
+let loanTwoInterestCompounded: String
+
+// non utilization rate variables
+let nonUtilizationInterestAmount: number
+let dailyNonUtilizationInterest: number
+let dailyNonUtilizationRate: number
+let nonUtilizationInterestServiced: String
+let nonUtilizationInterestCompounded: String
+
+// interest variables
+let interestServiced: String
+let loanInterestRate: number
+let minimumInterestAmount: number
+
+let totalInterestDue: number
+let totalInterestPaid: number
+let totalInterestCharged: number
+let totalInterestNotDue: number
+let totalInterestOutstanding: number
+let totalInterest: number
+
+let balancingInterestCharged: number
+let balancingInterestDue: number
+let balancingInterestOutstanding: number
+
+// fees variables
+let lendingFees: number
+let lendingFeePercentage: number
+let lendingFeeAddedToLoan: String
+
+let otherFeesPayable: number
+let OtherFeesAddedToLoan: String
+
+let feeCharged: number
+let feeDue: number
+let feePaid: number
+let feeOutstanding: number
+
+// exit fee variables
+let exitFeeAmount: number
+let exitFeeGDV: number
+let exitFeeLoan: number
+
+// general loan variables
+let facilityAmount: number
+let totalLoan: number
+let loanType: number
+let drawdownOneAmount: number
+let drawdownAmountTwo: number
+let undrawnBalance: number
+let GDV: number
+let TotalInterestCharged: number
+let TotalInterestDue: number
+let TotalFeesDue: number
+let BalanceOfLoanOutstanding: number
+let TotalRedemptionAmount: number
+
+
+
+export function loan() {
+    // define date variables        
+    let date = moment().format('d-MMM-YYYY');
+    let midnight = moment().startOf('day').format();
+    let now = moment().format();
+    // On first drawdown date
+    if (date === startDate) {
+        drawdownAmount()
+        lendingFee()
+        otherFees()
+        // Every day on midnight
+    } else if (now === midnight) {
+        // on second or third drawdown date
+        for (let ddate of drawdownDate) {
+            if (ddate === date) {
+                drawdownAmount()
+            }
+
+        }
+        // for every other midnight
+        loanOneBalance()
+        loanTwoBalance()
+        nonUtilizationInterest()
+
+        // on repayment date
+    } else if (date === repaymentDate) {
+        minimumInterest()
+        exitFees()
+        TotalInterestCharged = totalInterestCharged + balancingInterestDue
+        TotalInterestDue = totalInterestDue
+        BalanceOfLoanOutstanding = balanceOfLoanOne + balanceOfLoanTwo
+        TotalRedemptionAmount = BalanceOfLoanOutstanding + totalInterestDue + TotalFeesDue
+
+    }
+}
+
+
+// check on drawdown dates
+export function drawdownAmount() {
+    undrawnBalance = facilityAmount
+    if (loanType === 1) {
+        drawdownOneAmount = drawdownOneAmount + balanceOfLoanOne
+        undrawnBalance = undrawnBalance - drawdownOneAmount
+
+    } else if (loanType === 2) {
+        drawdownOneAmount = drawdownOneAmount + balanceOfLoanTwo
+        undrawnBalance = undrawnBalance - drawdownOneAmount
+
+    }
+
+
+}
+// calculate lending fees on first drawdown date
+export function lendingFee() {
+    if (interestServiced === "yes") {
+        lendingFees = totalLoan * (lendingFeePercentage / 100)
+    } else if (interestServiced === "no") {
+        lendingFees = (totalLoan * loanInterestRate / 100) * (lendingFeePercentage / 100)
+        if (lendingFeeAddedToLoan === "yes") {
+            feeDue = feeDue + lendingFees
+            feePaid = feePaid + lendingFees
+            balanceOfLoanOne = balanceOfLoanOne + lendingFees
+        } else if (lendingFeeAddedToLoan === "no") {
+            feeCharged = feeCharged + lendingFees
+            feeDue = feeDue + lendingFees
+            feePaid = feePaid + lendingFees
+            feeOutstanding = feeOutstanding + lendingFees
+        }
+    }
+
+}
+
+// calculate other fees on first drawdown date
+export function otherFees() {
+    if (OtherFeesAddedToLoan === "yes") {
+        feeDue = feeDue + otherFeesPayable
+        feePaid = feePaid + otherFeesPayable
+        balanceOfLoanOne = balanceOfLoanOne + otherFeesPayable
+    } else if (OtherFeesAddedToLoan === "no") {
+        feeDue = feeDue + otherFeesPayable
+        feePaid = feePaid + otherFeesPayable
+        feeOutstanding = feeOutstanding + otherFeesPayable
+        feeCharged = feeCharged + otherFeesPayable
+
+    }
+}
+// calculate loan type one balance
+export function loanOneBalance() {
+
+    dailyOneInterest = balanceOfLoanOne * dailyRateOfLoanOne
+
+    if (loanOneInterestServiced === "yes") {
+        totalInterestCharged = totalInterestCharged + dailyOneInterest
+        totalInterestDue = totalInterestDue + dailyOneInterest
+        totalInterestPaid = totalInterestPaid + dailyOneInterest
+        totalInterestOutstanding = totalInterestOutstanding + dailyOneInterest
+    } else if (loanOneInterestServiced === "no") {
+        if (loanOneInterestComponded === "yes") {
+            totalInterestCharged = totalInterestCharged + dailyOneInterest
+            totalInterestDue = totalInterestDue + dailyOneInterest
+            totalInterestPaid = totalInterestPaid + dailyOneInterest
+            balanceOfLoanOne = balanceOfLoanOne + dailyOneInterest
+        } else if (loanOneInterestComponded === "no") {
+            totalInterestCharged = totalInterestCharged + dailyOneInterest
+            totalInterestNotDue = totalInterestNotDue + dailyOneInterest
+            totalInterestOutstanding = totalInterestOutstanding + dailyOneInterest
+
+        }
+    }
+}
+
+// calculate loan type two balance
+export function loanTwoBalance() {
+    dailyTwoInterest = balanceOfLoanTwo * dailyRateOfLoanTwo
+
+    if (loanTwoInterestServiced === "yes") {
+        totalInterestCharged = totalInterestCharged + dailyTwoInterest
+        totalInterestDue = totalInterestDue + dailyTwoInterest
+        totalInterestPaid = totalInterestPaid + dailyTwoInterest
+        totalInterestOutstanding = totalInterestOutstanding + dailyTwoInterest
+    } else if (loanTwoInterestServiced === "no") {
+        if (loanTwoInterestCompounded === "yes") {
+            totalInterestCharged = totalInterestCharged + dailyTwoInterest
+            totalInterestDue = totalInterestDue + dailyTwoInterest
+            totalInterestPaid = totalInterestPaid + dailyTwoInterest
+            balanceOfLoanTwo = balanceOfLoanTwo + dailyTwoInterest
+        } else if (loanTwoInterestCompounded === "no") {
+            totalInterestCharged = totalInterestCharged + dailyTwoInterest
+            totalInterestNotDue = totalInterestNotDue + dailyTwoInterest
+            totalInterestOutstanding = totalInterestOutstanding + dailyTwoInterest
+
+        }
+    }
+}
+
+// calculate non utilization interest
+export function nonUtilizationInterest() {
+    dailyNonUtilizationInterest = undrawnBalance * dailyNonUtilizationRate
+    if (nonUtilizationInterestServiced === "yes") {
+        totalInterestCharged = totalInterestCharged + dailyNonUtilizationInterest
+        totalInterestDue = totalInterestDue + dailyNonUtilizationInterest
+        totalInterestPaid = totalInterestPaid + dailyNonUtilizationInterest
+        totalInterestOutstanding = totalInterestOutstanding + dailyNonUtilizationInterest
+    } else if (nonUtilizationInterestServiced === "no") {
+        if (nonUtilizationInterestCompounded === "yes") {
+            totalInterestCharged = totalInterestCharged + dailyNonUtilizationInterest
+            totalInterestDue = totalInterestDue + dailyNonUtilizationInterest
+            totalInterestPaid = totalInterestPaid + dailyNonUtilizationInterest
+            balanceOfLoanOne = balanceOfLoanTwo + dailyNonUtilizationInterest
+        } else if (nonUtilizationInterestCompounded === "no") {
+            totalInterestCharged = totalInterestCharged + dailyNonUtilizationInterest
+            totalInterestNotDue = totalInterestNotDue + dailyNonUtilizationInterest
+            totalInterestOutstanding = totalInterestOutstanding + dailyNonUtilizationInterest
+
+        }
+    }
+}
+
+// check whether minimum interest is met
+export function minimumInterest() {
+    if (totalInterestCharged !== minimumInterestAmount) {
+        balancingInterestCharged = minimumInterestAmount - totalInterestCharged
+        balancingInterestDue = minimumInterestAmount - totalInterestCharged
+        balancingInterestOutstanding = minimumInterestAmount - totalInterestCharged
+
+    }
+
+}
+// check for exit fees
+export function exitFees() {
+    if (exitFeeAmount !== 0) {
+        feeDue = feeDue + exitFeeAmount
+    }
+    if (exitFeeGDV !== 0) {
+        exitFeeAmount = (exitFeeGDV / 100) * GDV
+        feeDue = feeDue + exitFeeAmount
+        feeOutstanding = feeOutstanding + exitFeeAmount
+    }
+    if (exitFeeLoan !== 0) {
+        exitFeeAmount = (totalLoan + totalInterestCharged) * (exitFeeLoan / 100)
+        feeCharged = feeCharged + exitFeeAmount
+        feeDue = feeDue + exitFeeAmount
+        feeOutstanding = feeOutstanding + exitFeeAmount
+
+    }
+
+}
--- test/loanlibrary.test.ts
+++ test/loanlibrary.test.ts
@@ -1,14 +1,163 @@
-import DummyClass from '../src/loanlibrary'
+// import DummyClass from '../src/loanlibrary'
 
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
+// describe('Dummy test', () => {
+//   it('works if true is truthy', () => {
+//     expect(true).toBeTruthy()
+//   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+//   it('DummyClass is instantiable', () => {
+//     expect(new DummyClass()).toBeInstanceOf(DummyClass)
+//   })
+// })
+
+import {drawdownAmount, lendingFee, otherFees, loanOneBalance, loanTwoBalance, nonUtilizationInterest,minimumInterest, exitFees} from "../src/loanlibrary";
+
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+let startDate: String = "2-Oct-2018"
+let repaymentDate: String = "1-Dec-2018"
+let drawdownDate: any = ["5-Oct-2018", "8-Nov-2018", "10-Nov-2018"]
+
+// loan type one variables
+let dailyOneInterest: number = 0
+let dailyRateOfLoanOne: number = 0.03
+let balanceOfLoanOne: number = 0
+let loanOneInterestServiced: String = "no"
+let loanOneInterestComponded: String = "yes"
+
+// loan type two variables
+let dailyTwoInterest: number = 1
+let dailyRateOfLoanTwo: number = 0.03
+let balanceOfLoanTwo: number = 0
+let loanTwoInterestServiced: String = "yes"
+let loanTwoInterestCompounded: String = "no"
+
+// non utilization rate variables
+let nonUtilizationInterestAmount: number = 0
+let dailyNonUtilizationInterest: number = 0
+let dailyNonUtilizationRate: number = 0.07
+let nonUtilizationInterestServiced: String = ""
+let nonUtilizationInterestCompounded: String = "yes"
+
+// default
+let defaultRate: number = 2
+let defaultCompounded: string = "yes"
+let capitalizeLoanOnDefault: string = "yes"
+let defaultAtEndOfTerm: string = "yes"
+
+// interest variables
+let interestServiced: String = "no"
+let loanInterestRate: number = 1
+let minimumInterestAmount: number = 1500
+
+let totalInterestDue: number = 0
+let totalInterestPaid: number = 0
+let totalInterestCharged: number = 0
+let totalInterestNotDue: number = 0
+let totalInterestOutstanding: number = 0
+let totalInterest: number = 0
+
+let balancingInterestCharged: number = 0
+let balancingInterestDue: number = 0
+let balancingInterestOutstanding: number = 0
+
+// fees variables
+let lendingFees: number = 0
+let lendingFeePercentage: number = 2
+let lendingFeeAddedToLoan: String = "yes"
+
+let otherFeesPayable: number = 5000
+let OtherFeesAddedToLoan: String = "yes"
+
+let feeCharged: number = 0
+let feeDue: number = 0
+let feePaid: number = 0
+let feeOutstanding: number = 0
+
+// exit fee variables
+let exitFeeAmount: number = 0
+let exitFeeGDV: number = 2
+let exitFeeLoan: number = 0
+
+// general loan variables
+let facilityAmount: number = 10000
+let totalLoan: number = 0
+let loanType: number = 1
+let drawdownOneAmount: number = 50000
+let drawdownAmountTwo: any = [25000, 25000]
+let undrawnBalance: number = 0
+let GDV: number = 250000
+let TotalInterestCharged: number = 0
+let TotalInterestDue: number = 0
+let TotalFeesDue: number = 0
+let BalanceOfLoanOutstanding: number = 0
+let TotalRedemptionAmount: number = 0
+
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(drawdownAmount()).to.equal(7);
+  });
+
+});
+
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(lendingFee()).to.equal(7);
+  });
+
+});
+
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(otherFees()).to.equal(7);
+  });
+
+});
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(loanOneBalance()).to.equal(7);
+  });
+
+});
+
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(loanTwoBalance()).to.equal(7);
+  });
+
+});
+
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(nonUtilizationInterest()).to.equal(7);
+  });
+
+});
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(minimumInterest()).to.equal(7);
+  });
+
+});
+describe('loanlibrary', () => {
+
+  it('should be able to ', () => {
+    expect(exitFees()).to.equal(7);
+  });
+
+});
+
+
