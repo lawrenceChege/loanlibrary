3e3c1fb23f164c9d9f27562bf81a1b85a455b53e
lawrenceChege <lawrencechege.s@gmail.com>
2018-10-18 03:16:25 +0300

    test for drawdown fixed

--- src/loanlibrary.ts
+++ src/loanlibrary.ts
@@ -372,49 +372,25 @@ export class MAIN extends LOAN {
     }
 }
 
-export interface NUMBERS {
-    x : number
-    y: number
-    z: number
-    sum: number
-
-}
-export class MATH implements NUMBERS {
-    x : number
-    y: number
-    z: number
-    sum: number
-    constructor(x : number,y: number,z: number, sum: number){
-        this.x = x
-        this.y = y
-        this.z = z
-        this.sum = sum
-    }
-
-}
-export class ADD extends MATH{
-    getSum(){
-        this.sum = this.x + this.y;
-        return this.sum
-    }
-}
 
 export class DRAWDOWN extends LOAN{
     getDrawdownAmount (undrawnBalance: number, facilityAmount:number, loanType:number, balanceOfLoanOne:number){
     this.undrawnBalance = this.facilityAmount
-    // console.log(facilityAmount)
-    // console.log(this.facilityAmount)
+    console.log([" Undrawn balance", this.undrawnBalance])
     if (this.loanType === 1) {
         this.drawdownOneAmount += this.balanceOfLoanOne
         this.undrawnBalance -= this.drawdownOneAmount
+        console.log([" First Draw down Amount", this.drawdownOneAmount])
         return this.drawdownOneAmount;
 
     } else if (this.loanType === 2) {
         this.drawdownOneAmount += this.balanceOfLoanTwo
         this.undrawnBalance -= this.drawdownOneAmount
+        console.log([" First Draw down Amount", this.drawdownOneAmount])
         return this.drawdownOneAmount;
 
     }
+    return this.undrawnBalance;
 
     }
 }
